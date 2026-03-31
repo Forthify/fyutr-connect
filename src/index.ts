3ea3a8680cbb897f897b6d6d4ee4526d48ae7df2
xinyitoh <xinyitoh74@gmail.com>
2026-03-31 16:48:46 +0800

    feat: add APU institution integration

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { iiumRoute } from "./institution/iium/route";
 import { iicRoute } from "./institution/iic/route";
 import { uitmRoute } from "./institution/uitm/route";
 import { contributorsRoute } from "./routes/contributors/route";
+import { apuRoute } from "./institution/apu/route";
 
 const app = new Hono();
 
 app.route("/institution/iium", iiumRoute);
 app.route("/institution/iic", iicRoute);
 app.route("/institution/uitm", uitmRoute);
+app.route("/institution/apu", apuRoute);
 app.route("/contributors", contributorsRoute);
 
 app.get("/", (c) => {
