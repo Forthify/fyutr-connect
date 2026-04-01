84dd38c9568d5fe86becfbca4adb8bea754234c4
Muhammad Nur Irfan Bin Mohd Ariff <mnifanmohdariff@gmail.com>
2026-04-01 20:29:55 +0800

    feat: Add UniKL (Universiti Kuala Lumpur) Integration (#2)
    * feat: add UniKL integration support
    
    * fix: resolve typescript import error and implicit type

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { iicRoute } from "./institution/iic/route";
 import { uitmRoute } from "./institution/uitm/route";
 import { contributorsRoute } from "./routes/contributors/route";
 import { apuRoute } from "./institution/apu/route";
+import { uniklRoute } from "./institution/unikl/route";
 
 const app = new Hono();
 
@@ -11,6 +12,7 @@ app.route("/institution/iium", iiumRoute);
 app.route("/institution/iic", iicRoute);
 app.route("/institution/uitm", uitmRoute);
 app.route("/institution/apu", apuRoute);
+app.route("/institution/unikl", uniklRoute);
 app.route("/contributors", contributorsRoute);
 
 app.get("/", (c) => {
